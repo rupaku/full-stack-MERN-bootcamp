b74481731224950ca6cf3d32db8dd62d82ca24bb
rupaku <rupakumari201@yahoo.com>
2020-11-28 22:20:32 +0530

    CRUD operation on category

--- projbackend/app.js
+++ projbackend/app.js
@@ -12,6 +12,7 @@ const cors = require("cors")
 const authRoutes = require("./routes/auth")
 
 const userRoutes = require("./routes/user")
+const categoryRoutes = require("./routes/category")
 
 //DB connection
 mongoose
@@ -32,6 +33,7 @@ app.use(cors())
 //My routes
 app.use("/api", authRoutes)
 app.use("/api", userRoutes)
+app.use("/api", categoryRoutes)
 
 //PORT
 const port = process.env.PORT || 8000
